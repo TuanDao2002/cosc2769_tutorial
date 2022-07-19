e8751c23f2224c971e217e53746e826380e9588e
TuanDao2002 <tuan09092002@gmail.com>
2022-07-19 12:01:10 +0700

    update

--- Tutorial 4/myapp/src/Hello.js
+++ Tutorial 4/myapp/src/Hello.js
@@ -1,3 +1,16 @@
-export default function Hello({name, address}) {
-    return <span>Hello, {name}, my address is {address}</span>
+import { useState } from "react";
+
+export default function Hello({ address }) {
+	const [name, setName] = useState("");
+	const changeName = () => {
+		setName("tuan");
+	};
+	return (
+		<div>
+			Hello, {name}, my address is {address}
+			<div>
+				<button onClick={() => changeName()}>Change</button>
+			</div>
+		</div>
+	);
 }
